0a1285e64fe8bcbdafb6315916c61e82c07a9f22
Ein Menthos <59303591+EinMenthos@users.noreply.github.com>
2023-04-20 14:54:47 -0700

    handling missing arguments

--- server.py
+++ server.py
@@ -3,40 +3,41 @@
 # Server code
 import socket
 import sys
+try:
+    # Gets the port number of the server
+    serverPort = int(sys.argv[1])
 
-# Gets the port number of the server
-serverPort = int(sys.argv[1])
+    # Create a TCP socket
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create a TCP socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    serverSocket.bind(('', serverPort))
 
-# Bind the socket to the port
-serverSocket.bind(('', serverPort))
+    # Start listening for incoming connections
+    serverSocket.listen(1)
 
-# Start listening for incoming connections
-serverSocket.listen(1)
+    print("The server is ready to receive")
+    # The buffer to store the received data
+    data = ""
 
-print("The server is ready to receive")
-# The buffer to store the received data
-data = ""
+    # Forever accept incoming connections
+    while 1:
+    # Accept a connection ; get client’ s socket
+        connectionSocket, addr = serverSocket.accept()
+        print("connection from " + socket.gethostname())
+        # The temporary buffer
+        tmpBuff = ""
+        while len(data) != 40:
+            tmpBuff = connectionSocket.recv(40).decode()
+            # print(tmpBuff)
+    # The other side unexpectedly closed it’s socket
+            if not tmpBuff:
+                break
+    # Save the data
+            data += tmpBuff
+        print(data)
 
-# Forever accept incoming connections
-while 1:
- # Accept a connection ; get client’ s socket
-    connectionSocket, addr = serverSocket.accept()
-    print("connection from " + socket.gethostname())
-    # The temporary buffer
-    tmpBuff = ""
-    while len(data) != 40:
-        tmpBuff = connectionSocket.recv(40).decode()
-        # print(tmpBuff)
-# The other side unexpectedly closed it’s socket
-        if not tmpBuff:
-            break
-# Save the data
-        data += tmpBuff
-    print(data)
-
-    # Close the socket
-    connectionSocket.close()
-print("end of server")
+        # Close the socket
+        connectionSocket.close()
+except:
+    print("Mission arguments: port")
