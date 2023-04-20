a92e39158978b038bfa4dc0ecf5c26daa0d2bb72
DYHanzawa-777 <dtyoshid@csu.fullerton.edu>
2023-04-19 23:32:50 -0700

    Updated how to get the arguments for the client and server

--- client.py
+++ client.py
@@ -1,39 +1,35 @@
-#python client.py 127.0.0.1 12001
+# python client.py 127.0.0.1 12001
 
 # Client code
 import socket
-import argparse
+import sys
 
-# Create the parser
-parser = argparse.ArgumentParser()
-# Add an argument
-parser.add_argument('name', type=str)
-parser.add_argument('port', type=int)
-# Parse the argument
-args = parser.parse_args()
-# Name and port number of the server to which want to connect .
-serverPort = args.port
-serverName = args.name
+# Sends the address of the server
+serverAddress = str(sys.argv[1])
+
+# Sends the port number of the server
+serverPort = int(sys.argv[2])
 
 # Create a socket
-clientSocket = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Connect to the server
 try:
-    clientSocket.connect (( serverName , serverPort ))
+    clientSocket.connect((serverAddress, serverPort))
     # A string we want to send to the server
     data = "Hello world! This is a very long string. "
     bytesSent = 0
     print("trying to send")
 
     # Keep sending bytes until all bytes are sent
-    while bytesSent != len ( data ) :
+    while bytesSent != len(data):
         # Send that string !
         print("sending message")
-        bytesSent += clientSocket.send (data.encode()) 
-        #bytesSent += clientSocket.send(data[bytes sent])
-    
+        bytesSent += clientSocket.send(data.encode())
+        # bytesSent += clientSocket.send(data[bytes sent])
+
 except:
     print("Connection to the server could not be established.\nCheck IP and PORT from the server.")
 
 # Close the socket
-clientSocket.close ()
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,47 +1,42 @@
-#python server.py 12001
+# python server.py 12001
 
 # Server code
 import socket
-import argparse
+import sys
 
-# Create the parser
-parser = argparse.ArgumentParser()
-# Add an argument
-parser.add_argument('port', type=int)
-# Parse the argument
-args = parser.parse_args()
-#print(args.port)
-serverPort = args.port
+# Gets the port number of the server
+serverPort = int(sys.argv[1])
 
+# Create a TCP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
- # Create a TCP socket
-serverSocket = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
- # Bind the socket to the port
-serverSocket.bind(('',serverPort ))
- # Start listening for incoming connections
-serverSocket.listen ( 1 )
+# Bind the socket to the port
+serverSocket.bind(('', serverPort))
 
-print ("The server is ready to receive")
-# The buffer to s tore the received data
+# Start listening for incoming connections
+serverSocket.listen(1)
+
+print("The server is ready to receive")
+# The buffer to store the received data
 data = ""
 
- # Forever accept incoming connections
-while 1 :
+# Forever accept incoming connections
+while 1:
  # Accept a connection ; get client’ s socket
-    connectionSocket , addr = serverSocket . accept ()
+    connectionSocket, addr = serverSocket.accept()
     print("connection from " + socket.gethostname())
     # The temporary buffer
     tmpBuff = ""
-    while len ( data ) != 40 :
-        tmpBuff = connectionSocket.recv (40).decode()
-        #print(tmpBuff)
+    while len(data) != 40:
+        tmpBuff = connectionSocket.recv(40).decode()
+        # print(tmpBuff)
 # The other side unexpectedly closed it’s socket
-        if not tmpBuff :
+        if not tmpBuff:
             break
 # Save the data
         data += tmpBuff
-    print (data)
+    print(data)
 
     # Close the socket
-    connectionSocket.close ()
+    connectionSocket.close()
 print("end of server")
