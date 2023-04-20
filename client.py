601aa1e80f4f8bf0759cb6878120fb5f2f00cabe
Ein Menthos <59303591+EinMenthos@users.noreply.github.com>
2023-04-19 19:51:45 -0700

    added arguments
    usage:
    
    python client.py 127.0.0.1 12001

--- client.py
+++ client.py
@@ -1,26 +1,39 @@
+#python client.py 127.0.0.1 12001
 
 # Client code
 import socket
+import argparse
 
+# Create the parser
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('name', type=str)
+parser.add_argument('port', type=int)
+# Parse the argument
+args = parser.parse_args()
 # Name and port number of the server to which want to connect .
-#serverName = 'ecs.fullerton.edu'
-#serverName = '10.67.86.205'
-serverName = '127.0.0.1'
-serverPort = 12000
+serverPort = args.port
+serverName = args.name
+
 # Create a socket
 clientSocket = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
 # Connect to the server
-clientSocket.connect (( serverName , serverPort ))
-# A string we want to send to the server
-data = "Hello world! This is a very long string. "
-bytesSent = 0
+try:
+    clientSocket.connect (( serverName , serverPort ))
+    # A string we want to send to the server
+    data = "Hello world! This is a very long string. "
+    bytesSent = 0
+    print("trying to send")
 
-# Keep sending bytes until all bytes are sent
-while bytesSent != len ( data ) :
-# Send that string !
-    print("sending message")
-    bytesSent += clientSocket.send (data.encode()) 
-    #bytesSent += clientSocket.send(data[bytes sent])
-# Close the s ocket
+    # Keep sending bytes until all bytes are sent
+    while bytesSent != len ( data ) :
+        # Send that string !
+        print("sending message")
+        bytesSent += clientSocket.send (data.encode()) 
+        #bytesSent += clientSocket.send(data[bytes sent])
+    
+except:
+    print("Connection to the server could not be established.\nCheck IP and PORT from the server.")
+
+# Close the socket
 clientSocket.close ()
-print("end of this program")
