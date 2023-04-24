af62f85dc71645f6299fa36c68c9ce953d7f3f50
DYHanzawa-777 <dtyoshid@csu.fullerton.edu>
2023-04-24 01:46:07 -0700

    Serv.py has error handling for command line as well

--- serv.py
+++ serv.py
@@ -5,115 +5,119 @@ import sys
 import os
 import subprocess
 
-# Gets the port number of the server
-serverPort = int(sys.argv[1])
+# Command line checks
+if len(sys.argv) < 2:
+    print("USAGE: python serv.py <port number>")
 
-# Create a TCP socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+else:
+    # Gets the port number of the server
+    serverPort = int(sys.argv[1])
 
-# Bind the socket to the port
-serverSocket.bind(('', serverPort))
+    # Create a TCP socket
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Start listening for incoming connections
-serverSocket.listen(1)
+    # Bind the socket to the port
+    serverSocket.bind(('', serverPort))
 
-# Buffer size
-bufferSize = 4096
+    # Start listening for incoming connections
+    serverSocket.listen(1)
 
-# ************************************************
-# Receives the specified number of bytes
-# from the specified socket
-# @param sock - the socket from which to receive
-# @param numBytes - the number of bytes to receive
-# @return - the bytes received
-# *************************************************
-def recvAll(sock, numBytes):
+    # Buffer size
+    bufferSize = 4096
 
-    # The buffer
-    recvBuff = ""
+    # ************************************************
+    # Receives the specified number of bytes
+    # from the specified socket
+    # @param sock - the socket from which to receive
+    # @param numBytes - the number of bytes to receive
+    # @return - the bytes received
+    # *************************************************
+    def recvAll(sock, numBytes):
 
-    # The temporary buffer
-    tmpBuff = ""
+        # The buffer
+        recvBuff = ""
 
-    # Keep receiving till all is received
-    while len(recvBuff) < numBytes:
+        # The temporary buffer
+        tmpBuff = ""
 
-        # Attempt to receive bytes
-        tmpBuff = sock.recv(numBytes)
+        # Keep receiving till all is received
+        while len(recvBuff) < numBytes:
 
-        # The other side has closed the socket
-        if not tmpBuff:
+            # Attempt to receive bytes
+            tmpBuff = sock.recv(numBytes)
+
+            # The other side has closed the socket
+            if not tmpBuff:
+                break
+
+            # Add the received bytes to the buffer
+            recvBuff += tmpBuff
+
+        return recvBuff
+
+    # Accept connections forever
+    while True:
+
+        print("Waiting for connections...")
+
+        # Accept connections
+        clientSocket, addr = serverSocket.accept()
+
+        print("Accepted connection from client: ", addr)
+        print("\n")
+
+        receivedData = clientSocket.recv(bufferSize).decode()
+        print(receivedData)
+
+        ###################################################################################
+
+        if receivedData == "get":
+            print("SUCCESS")
+
+        ###################################################################################
+
+        elif receivedData == "put":
+            print("SUCCESS")
+
+        ###################################################################################
+
+        elif receivedData == "ls":
+            for line in subprocess.getstatusoutput(receivedData):
+                print(line)
+
+            print("SUCCESS")
+
+        else:
             break
 
-        # Add the received bytes to the buffer
-        recvBuff += tmpBuff
+        # The buffer to all data received from the
+        # the client.
+        fileData = ""
 
-    return recvBuff
+        # The temporary buffer to store the received
+        # data.
+        recvBuff = ""
 
+        # The size of the incoming file
+        fileSize = 0
 
-# Accept connections forever
-while True:
+        # The buffer containing the file size
+        fileSizeBuff = ""
 
-    print("Waiting for connections...")
+        # Receive the first 10 bytes indicating the
+        # size of the file
+        fileSizeBuff = recvAll(clientSocket, 10)
 
-    # Accept connections
-    clientSocket, addr = serverSocket.accept()
+        # Get the file size
+        fileSize = int(fileSizeBuff)
 
-    print("Accepted connection from client: ", addr)
-    print("\n")
+        print("The file size is ", fileSize)
 
-    receivedData = clientSocket.recv(bufferSize).decode()
-    print(receivedData)
+        # Get the file data
+        fileData = recvAll(clientSocket, fileSize)
 
-    ###################################################################################
+        print("The file data is: ")
+        print(fileData)
 
-    if receivedData == "get":
-        print("SUCCESS")
-
-    ###################################################################################
-
-    elif receivedData == "put":
-        print("SUCCESS")
-
-    ###################################################################################
-
-    elif receivedData == "ls":
-        for line in subprocess.getstatusoutput(receivedData):
-            print(line)
-
-        print("SUCCESS")
-
-    else:
-        break
-
-    # The buffer to all data received from the
-    # the client.
-    fileData = ""
-
-    # The temporary buffer to store the received
-    # data.
-    recvBuff = ""
-
-    # The size of the incoming file
-    fileSize = 0
-
-    # The buffer containing the file size
-    fileSizeBuff = ""
-
-    # Receive the first 10 bytes indicating the
-    # size of the file
-    fileSizeBuff = recvAll(clientSocket, 10)
-
-    # Get the file size
-    fileSize = int(fileSizeBuff)
-
-    print("The file size is ", fileSize)
-
-    # Get the file data
-    fileData = recvAll(clientSocket, fileSize)
-
-    print("The file data is: ")
-    print(fileData)
-
-    # Close our side
-    clientSocket.close()
+        # Close our side
+        clientSocket.close()
