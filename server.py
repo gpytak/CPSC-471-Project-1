a3f7ef5b7141c9c7f62b4b066c681c3098ffe5b1
DYHanzawa-777 <dtyoshid@csu.fullerton.edu>
2023-04-24 01:34:10 -0700

    Made some progress on the assignment

--- server.py
+++ server.py
@@ -1,43 +1,119 @@
 # python server.py 12001
 
-# Server code
 import socket
 import sys
-try:
-    # Gets the port number of the server
-    serverPort = int(sys.argv[1])
+import os
+import subprocess
 
-    # Create a TCP socket
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Gets the port number of the server
+serverPort = int(sys.argv[1])
 
-    # Bind the socket to the port
-    serverSocket.bind(('', serverPort))
+# Create a TCP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Start listening for incoming connections
-    serverSocket.listen(1)
+# Bind the socket to the port
+serverSocket.bind(('', serverPort))
 
-    print("The server is ready to receive")
-    # The buffer to store the received data
-    data = ""
+# Start listening for incoming connections
+serverSocket.listen(1)
 
-    # Forever accept incoming connections
-    while 1:
-    # Accept a connection ; get client’ s socket
-        connectionSocket, addr = serverSocket.accept()
-        print("connection from " + socket.gethostname())
-        # The temporary buffer
-        tmpBuff = ""
-        while len(data) != 40:
-            tmpBuff = connectionSocket.recv(40).decode()
-            # print(tmpBuff)
-    # The other side unexpectedly closed it’s socket
-            if not tmpBuff:
-                break
-    # Save the data
-            data += tmpBuff
-        print(data)
+# Buffer size
+bufferSize = 4096
 
-        # Close the socket
-        connectionSocket.close()
-except:
-    print("Mission arguments: port")
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
+
+    # The buffer
+    recvBuff = ""
+
+    # The temporary buffer
+    tmpBuff = ""
+
+    # Keep receiving till all is received
+    while len(recvBuff) < numBytes:
+
+        # Attempt to receive bytes
+        tmpBuff = sock.recv(numBytes)
+
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
+
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff
+
+    return recvBuff
+
+
+# Accept connections forever
+while True:
+
+    print("Waiting for connections...")
+
+    # Accept connections
+    clientSocket, addr = serverSocket.accept()
+
+    print("Accepted connection from client: ", addr)
+    print("\n")
+
+    receivedData = clientSocket.recv(bufferSize).decode()
+    print(receivedData)
+
+    ###################################################################################
+
+    if receivedData == "get":
+        print("SUCCESS")
+
+    ###################################################################################
+
+    elif receivedData == "put":
+        print("SUCCESS")
+
+    ###################################################################################
+
+    elif receivedData == "ls":
+        for line in subprocess.getstatusoutput(receivedData):
+            print(line)
+
+        print("SUCCESS")
+
+    else:
+        break
+
+    # The buffer to all data received from the
+    # the client.
+    fileData = ""
+
+    # The temporary buffer to store the received
+    # data.
+    recvBuff = ""
+
+    # The size of the incoming file
+    fileSize = 0
+
+    # The buffer containing the file size
+    fileSizeBuff = ""
+
+    # Receive the first 10 bytes indicating the
+    # size of the file
+    fileSizeBuff = recvAll(clientSocket, 10)
+
+    # Get the file size
+    fileSize = int(fileSizeBuff)
+
+    print("The file size is ", fileSize)
+
+    # Get the file data
+    fileData = recvAll(clientSocket, fileSize)
+
+    print("The file data is: ")
+    print(fileData)
+
+    # Close our side
+    clientSocket.close()
